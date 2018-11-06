86d03175d3bfe3b09818bdb49ebfeacb617bca3b
futoshi.ishiguro <futo.ishiguro@gmail.com>
2018-11-06 12:06:35 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,7 +10,7 @@ class MicropostsController < ApplicationController
       redirect_to root_url
       
     else
-      @microposts = current_user.microposts.order("create_at DESC").page(params[:page])
+      @microposts = current_user.feed_microposts.order("create_at DESC").page(params[:page])
       flash.now[:danger] = "メッセージの投稿に失敗しました。"
       render "toppages/index"
       
